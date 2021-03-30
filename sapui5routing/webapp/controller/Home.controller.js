d6c518db31dce99b59b75da1410e5151af62f751
Chandra Dutt <chandradutt.chandu@gmail.com>
2021-03-30 02:57:32 +0000

    Step 5 navigation without changing hash, added notfound target and controller

--- sapui5routing/webapp/controller/Home.controller.js
+++ sapui5routing/webapp/controller/Home.controller.js
@@ -4,5 +4,11 @@ sap.ui.define([
 ], function (BaseController) {
 	"use strict";
 	return BaseController.extend("sap.ui.demo.nav.controller.Home", {
+        	onDisplayNotFound : function () {
+			//display the "notFound" target without changing the hash
+			this.getRouter().getTargets().display("notFound", {
+				fromTarget : "home"
+			});
+		}
 	});
 });
